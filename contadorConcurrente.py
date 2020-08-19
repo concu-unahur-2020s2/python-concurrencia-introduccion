0297022e174cba8f5b1951c7db30b8ce5864b751
Martin Acuña <msguser@gmail.com>
2020-08-19 16:11:54 -0300

    update con respuestas

--- contadorConcurrente.py
+++ contadorConcurrente.py
@@ -9,6 +9,7 @@ counter = 0
 def cuenta():
     global counter
 
+#contador +1 tantas veces como MAX_COUNT divido THREADS (500000) en este caso.
     for i in range(int(MAX_COUNT/THREADS)):
         counter += 1
 
@@ -16,13 +17,15 @@ def cuenta():
 
 threads = []
 
+# crea un hilo definido por el valor THREADS (2), en cada hilo llama a la funcion cuenta()
 for i in range(THREADS):
     t = threading.Thread(target=cuenta)
     threads.append(t)
     t.start()
 
+# como el contador es compartido el valor final es indefinido.
 for t in threads:
     t.join()
-
+print(threads)
 print(f"Valor del contador: {counter}")
 
